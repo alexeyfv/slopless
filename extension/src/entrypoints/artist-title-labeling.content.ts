459b1b67b85810b7b0d4725e3cdf2d86534282e8
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2026-05-18 19:23:11 +0300

    Update artist-title-labeling.content.ts

--- extension/src/entrypoints/artist-title-labeling.content.ts
+++ extension/src/entrypoints/artist-title-labeling.content.ts
@@ -1,7 +1,7 @@
 import { ArtistTitleLabelingService } from '../services/ArtistTitleLabelingService'
 
 export default defineContentScript({
-  matches: ['*://music.yandex.com/*'],
+  matches: ['*://music.yandex.com/*', '*://music.yandex.ru/*'],
   main: async function () {
     // To display the AI label on the artist page,
     // we need to use a separate algorithm that checks
